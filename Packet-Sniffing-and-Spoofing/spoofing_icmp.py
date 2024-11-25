5caec54c8c0939d8613a579fa696e6f805605f62
Firat Kilic <firat.kilic55600@gmail.com>
2024-11-25 15:34:21 +0300

    first commit

---
+++ Packet-Sniffing-and-Spoofing/spoofing_icmp.py
@@ -0,0 +1,31 @@
+from scapy.all import *
+import argparse
+import random
+import time
+
+
+
+def send_fake_icmp_packets(target_ip,source_ip, count):
+	for _ in range(count):
+		packet = IP(src=source_ip, dst=target_ip) / ICMP()
+		send(packet)
+		print(f"Sent packet from {source_ip} to {target_ip}")
+		time.sleep(1)  
+
+def get_user_input():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-t","--target",dest="target",help="to set the destination ip")
+	parser.add_argument("-s","--source",dest="source",help="to set the source ip")
+	parser.add_argument("-c","--count",dest="count",type=int, default=10,help="to set the number of packets to send")
+	return parser.parse_args()
+	
+user_options = get_user_input()
+user_target = user_options.target
+user_source = user_options.source
+user_count = user_options.count		
+
+send_fake_icmp_packets(user_target,user_source, user_count)
+
+
+
+
